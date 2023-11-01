8f8a00d6e09fdddb86746732c7152bd432f2c496
Daniel Alm D'Adda <swedmarine@hotmail.com>
2023-11-01 09:06:16 +0100

    Fixed imports. Altered test position.

--- Pylandia/app.py
+++ Pylandia/app.py
@@ -1,14 +1,14 @@
 import os
 import sys
+# from Pylandia.Components.combat import test_combat # Import to test combat
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-from Entities.common.Combat import run_game
-
-
 def run():
-    run_game()
+    print('Game is running!')
 
 if __name__ == '__main__':
     run()
+    # test_combat() # Import to test combat
